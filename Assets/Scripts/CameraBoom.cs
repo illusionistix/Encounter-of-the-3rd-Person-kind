f23fa69d89ef45db8a05c3fe8bcb348adec76f87
illusionistix <86018686+illusionistix@users.noreply.github.com>
2022-09-08 21:11:01 +0200

    stuttering on jet cam fix
    Moved the jet cam position/rotation from Update to the FixedUpdate method.

--- Assets/Scripts/CameraBoom.cs
+++ Assets/Scripts/CameraBoom.cs
@@ -8,8 +8,8 @@ public class CameraBoom : MonoBehaviour
     [SerializeField] private Slider mouseSensitivitySlider;
 
     [SerializeField] private Transform playerCamTransform;
-    [SerializeField] private Transform playerObjectTransform;
-    [SerializeField] private Transform jetObjectTransform;
+    [SerializeField] private Rigidbody playerObjectRbd;
+    [SerializeField] private Rigidbody jetObjectRbd;
 
     //private bool isJetCamIntro;
     //private Vector3 startPos;
@@ -29,24 +29,33 @@ public class CameraBoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MainManager.Instance.mouseSensitivity = mouseSensitivitySlider.value;
+
         if (MainManager.Instance.isGameActive)
         {
             if (MainManager.Instance.isPlayerInJet)
-            {                
-                UseJetCamBoom();                
+            {
+                UseJetCamBoom();
             }
 
             if (!MainManager.Instance.isPlayerInJet)
-            {                
+            {
                 UsePlayerCamBoom();
                 //isJetCamIntro = false;
             }
-            
+
         }
+    }
 
-        MainManager.Instance.mouseSensitivity = mouseSensitivitySlider.value;
 
-
+    private void FixedUpdate()
+    {
+        if (MainManager.Instance.isPlayerInJet)
+        {
+            transform.position = jetObjectRbd.transform.position;
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, jetObjectRbd.transform.rotation, Time.deltaTime * 2f);
+        }        
     }
 
 
@@ -65,8 +74,9 @@ public class CameraBoom : MonoBehaviour
         playerCamTransform.localEulerAngles = new Vector3(-camVerticalAngle, 0, 0);
         playerCamTransform.localPosition = new Vector3(3f, 7f, -12.5f);
 
-        //transform.position = Vector3.Lerp(transform.position, playerObjectTransform.position, Time.deltaTime * 4f);
-        transform.position = playerObjectTransform.position;
+        transform.position = playerObjectRbd.transform.position;
+
+        //transform.position = Vector3.Lerp(transform.position, playerObjectTransform.position, Time.deltaTime * 4f);        
     }
 
     
@@ -74,20 +84,12 @@ public class CameraBoom : MonoBehaviour
     private void UseJetCamBoom()
     {
         //transform.SetParent(jetObjectTransform);
-        transform.position = jetObjectTransform.position;
-        //transform.rotation = jetObjectTransform.rotation;
-        transform.rotation = Quaternion.Slerp(transform.rotation, jetObjectTransform.rotation, Time.deltaTime * 2f);
+        
         //transform.localPosition = new Vector3(0f, 5f, -15f);        
 
         playerCamTransform.localPosition = new Vector3(0f, 12f, -40f);
-        Vector3 targetDir = jetObjectTransform.position - playerCamTransform.position;
+        Vector3 targetDir = jetObjectRbd.transform.position - playerCamTransform.position;
         playerCamTransform.rotation = Quaternion.LookRotation(targetDir);
-
-        //while (Vector3.Distance(transform.position, jetObjectTransform.position) > 50f)
-        //{            
-            //transform.position = Vector3.Lerp(transform.position, jetObjectTransform.position, Time.deltaTime * 2f);            
-        //}        
-        
     }
 
 }
